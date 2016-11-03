29684ab65de5973fd801cb8ede9f2b25cd866ef7
uriahrokach <uriahrokach@gmail.com>
2016-11-03 18:41:44 +0200

    code orginized

--- src/com/spikes2212/robot/subsystems/Drivetrain.java
+++ src/com/spikes2212/robot/subsystems/Drivetrain.java
@@ -8,28 +8,27 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class Drivetrain extends Subsystem {
-    
-	private Gearbox leftGearbox; 
+
+	private Gearbox leftGearbox;
 	private Gearbox rightGearbox;
-	
-	public Drivetrain(Gearbox leftGearbox, Gearbox rightGearbox){
+
+	public Drivetrain(Gearbox leftGearbox, Gearbox rightGearbox) {
 		this.leftGearbox = leftGearbox;
 		this.rightGearbox = rightGearbox;
 	}
-	
-	public void drive(double speed){
+
+	public void drive(double speed) {
 		leftGearbox.setSpeed(-speed);
 		rightGearbox.setSpeed(speed);
 	}
-	
-	public void turn(double speed){
+
+	public void turn(double speed) {
 		leftGearbox.setSpeed(speed);
 		rightGearbox.setSpeed(speed);
 	}
-	
-    public void initDefaultCommand() {
 
-    }
-    
+	public void initDefaultCommand() {
+
+	}
+
 }
-
