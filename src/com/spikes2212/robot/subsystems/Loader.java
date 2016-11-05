bd755f3c60e93771ed7c53de5faa8b1472932c9e
Spikes-2212 <spikes.no2212@gmail.com>
2016-11-05 10:50:59 +0200

    Loader added

---
+++ src/com/spikes2212/robot/subsystems/Loader.java
@@ -0,0 +1,34 @@
+package com.spikes2212.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Loader extends Subsystem {
+	private DigitalInput loadingLimit;
+	private VictorSP picker;
+
+	public Loader(DigitalInput loadingLimit, VictorSP picker) {
+		this.loadingLimit = loadingLimit;
+		this.picker = picker;
+	}
+
+	public boolean isLoaded() {
+		return !loadingLimit.get();
+	}
+
+	public void pick(double speed) {
+		while (!isLoaded()) {
+			picker.set(speed);
+		}
+			picker.set(0);
+	}
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+}
