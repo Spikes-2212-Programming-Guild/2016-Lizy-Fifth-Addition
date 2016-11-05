a81040883429cc28abdaaafd53000c0db969b73d
Spikes-2212 <spikes.no2212@gmail.com>
2016-11-05 11:15:41 +0200

    Changed from VictorSP to CANTalon

--- src/com/spikes2212/robot/subsystems/Shooter.java
+++ src/com/spikes2212/robot/subsystems/Shooter.java
@@ -1,16 +1,15 @@
 package com.spikes2212.robot.subsystems;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Shooter extends Subsystem {
-	private VictorSP shootingMotor;
+	private CANTalon shootingMotor;
 	
-	public Shooter(DigitalInput loadingLimit, VictorSP picker,VictorSP shootingMotor) {
+	public Shooter(CANTalon shootingMotor) {
 		this.shootingMotor = shootingMotor;
 	}
 	public void shoot(double speed){
