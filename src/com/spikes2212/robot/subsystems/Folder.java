de22adcaa73c9cba0df68511374acf6d51321593
uriahrokach <uriahrokach@gmail.com>
2016-11-05 13:57:28 +0200

    Reformatted

--- src/com/spikes2212/robot/subsystems/Folder.java
+++ src/com/spikes2212/robot/subsystems/Folder.java
@@ -8,28 +8,28 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class Folder extends Subsystem {
-    
-    private VictorSP folderMotor;
-    
-    private DigitalInput up;
+
+	private VictorSP folderMotor;
+
+	private DigitalInput up;
 	private DigitalInput down;
-    
-    public Folder(VictorSP folderMotor, DigitalInput up, DigitalInput down){
-    	this.folderMotor = folderMotor;
-    	
-    	this.up = up;
+
+	public Folder(VictorSP folderMotor, DigitalInput up, DigitalInput down) {
+		this.folderMotor = folderMotor;
+		this.up = up;
 		this.down = down;
-    }
-    public void turn(double speed){
-    	folderMotor.set(speed);
-    }
-    public boolean canMove(double speed) {
+	}
+
+	public void turn(double speed) {
+		folderMotor.set(speed);
+	}
+
+	public boolean canMove(double speed) {
 		return !(up.get() && speed > 0 || down.get() && speed < 0);
 	}
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
 }
-
